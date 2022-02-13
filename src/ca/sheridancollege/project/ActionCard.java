f35b05699246ed95dcf009b38cf1138cdef3cae6
justin0910 <josejust@sheridancollege.ca>
2022-02-12 21:40:44 -0500

    Create ActionCard.java

---
+++ src/ca/sheridancollege/project/ActionCard.java
@@ -0,0 +1,31 @@
+package ca.sheridancollege.project;
+
+// @author Justin Joseph
+public class ActionCard {
+    
+    //skipcard
+    public void skipCard(){
+
+    }
+
+    //reverse card
+    public void reverseCard(){
+
+    }
+
+    //2+ random color card
+    public void drawTwoPlusCard(){
+
+    }
+
+    //wild card
+    public void wildCard(){
+
+    }
+
+
+    //4+ wild card
+    public void drawFourWildCard(){
+
+    }
+}
