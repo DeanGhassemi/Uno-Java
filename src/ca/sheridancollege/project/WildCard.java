6e6d801d8464d698caca9f3abf3b7743cc7945ed
justin0910 <josejust@sheridancollege.ca>
2022-02-14 23:34:53 -0500

    Create WildCard.java

---
+++ src/ca/sheridancollege/project/WildCard.java
@@ -0,0 +1,28 @@
+package ca.sheridancollege.project;
+
+public class WildCard extends Card {
+    public WildCard(String colour) {
+        super(colour);
+        //TODO Auto-generated constructor stub
+    }
+
+    private boolean WildFour;
+    public void useCard(){
+
+    }   
+    
+    public boolean voidWildFour(){
+        return true;
+    }
+
+    public void setWildFour(boolean WildFour){
+
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    
+}
